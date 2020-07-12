54a87c466e1e45fb827e624606dabffceeacb009
Thor Kamphefner <thor314@gmail.com>
2020-07-12 16:30:56 +0200

    reinitiaziling vc

---
+++ config.el
@@ -0,0 +1,456 @@
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
+(use-package auto-compile
+  :config (auto-compile-on-load-mode))
+
+(setq load-prefer-newer t)
+
+(load-file "~/.emacs.d/sensible-defaults.el")
+(sensible-defaults/use-all-settings)
+(sensible-defaults/use-all-keybindings)
+(sensible-defaults/backup-to-temp-directory)
+
+(defun reload ()
+	"shorcut to reload init file"
+	(interactive)
+	(load-file "/Users/thor/.emacs.d/init.el"))
+
+(defun cedit ()
+	"shortcut to edit config.org file"
+	(interactive)
+	(find-file "/Users/thor/.emacs.d/config.org"))
+
+(defun tedit ()
+	"shortcut to edit org/tktodos.org"
+	(interactive)
+	(find-file "/Users/thor/org/tktodos.org"))
+
+(defun pedit ()
+	"shortcut to edit .profile"
+	(interactive)
+	(find-file "/Users/thor/.zprofile"))
+
+(defun describe-in-popup (fn)
+  (let* ((thing (symbol-at-point))
+         (description (save-window-excursion
+                        (funcall fn thing) ;; This is the yield point
+                        (switch-to-buffer "*Help*")
+                        (buffer-string))))
+    (popup-tip description
+               :point (point)
+               :around t
+               :height 30
+               :scroll-bar t
+               :margin t)))
+(defun describe-thing-in-popup ()
+  (interactive)
+  (let* ((thing (symbol-at-point)))
+    (cond
+     ((fboundp thing) (describe-in-popup 'describe-function))
+     ((boundp thing) (describe-in-popup 'describe-variable)))))
+
+(global-set-key (kbd "C-h C-h") 'describe-thing-in-popup)
+
+(defun hrs/rename-file (new-name)
+  (interactive "FNew name: ")
+  (let ((filename (buffer-file-name)))
+    (if filename
+        (progn
+          (when (buffer-modified-p)
+             (save-buffer))
+          (rename-file filename new-name t)
+          (kill-buffer (current-buffer))
+          (find-file new-name)
+          (message "Renamed '%s' -> '%s'" filename new-name))
+      (message "Buffer '%s' isn't backed by a file!" (buffer-name)))))
+
+(defun hrs/generate-scratch-buffer ()
+  "Create and switch to a temporary scratch buffer with a random
+     name."
+  (interactive)
+  (switch-to-buffer (make-temp-name "scratch-")))
+
+(defun hrs/kill-current-buffer ()
+  "Kill the current buffer without prompting."
+  (interactive)
+  (kill-buffer (current-buffer)))
+
+(defun hrs/add-auto-mode (mode &rest patterns)
+  "Add entries to `auto-mode-alist' to use `MODE' for all given file `PATTERNS'."
+  (dolist (pattern patterns)
+    (add-to-list 'auto-mode-alist (cons pattern mode))))
+
+(defun hrs/find-file-as-sudo ()
+  (interactive)
+  (let ((file-name (buffer-file-name)))
+    (when file-name
+      (find-alternate-file (concat "/sudo::" file-name)))))
+
+(defun hrs/region-or-word ()
+  (if mark-active
+      (buffer-substring-no-properties (region-beginning)
+                                      (region-end))
+    (thing-at-point 'word)))
+
+(defun hrs/append-to-path (path)
+  "Add a path both to the $PATH variable and to Emacs' exec-path."
+  (setenv "PATH" (concat (getenv "PATH") ":" path))
+  (add-to-list 'exec-path path))
+
+(defun hrs/insert-password ()
+  (interactive)
+  (shell-command "pwgen 30 -1" t))
+
+(defun hrs/notify-send (title message)
+  "Display a desktop notification by shelling out to `notify-send'."
+  (call-process-shell-command
+   (format "notify-send -t 2000 \"%s\" \"%s\"" title message)))
+
+(fset 'tk-org-insert-lisp-block
+   "#+begin_src emacs-lisp\C-m\C-m#+end_src\C-p")
+(global-set-key (kbd "<f2>") 'tk-org-insert-lisp-block)
+
+(fset 'tk-org-insert-rust-block
+   "#+begin_src rust\C-m\C-m#+end_src\C-p")
+(global-set-key (kbd "<f3>") 'tk-org-insert-rust-block)
+
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
+(setq make-backup-files nil) ; none of these~
+(setq auto-save-default nil) ; none of these#
+
+(use-package solarized-theme)
+(load-theme 'solarized-gruvbox-dark t)
+
+(defun set-frame-size-according-to-resolution ()
+  (interactive)
+  (if window-system
+  (progn
+    ;; use 120 char wide window for largeish displays
+    ;; and smaller 80 column windows for smaller displays
+    ;; pick whatever numbers make sense for you
+    (if (> (x-display-pixel-width) 1280)
+           (add-to-list 'default-frame-alist (cons 'width 100))
+           (add-to-list 'default-frame-alist (cons 'width 80)))
+    ;; for the height, subtract a couple hundred pixels
+    ;; from the screen height (for panels, menubars and
+    ;; whatnot), then divide by the height of a char to
+    ;; get the height we want
+    (add-to-list 'default-frame-alist
+         (cons 'height (/ (- (x-display-pixel-height) 60) ; close as I can get to full left half
+                             (frame-char-height)))))))
+
+(set-frame-size-according-to-resolution)
+
+(setq subword-mode t)
+
+(add-hook 'text-mode-hook 'turn-on-auto-fill) ;test
+      (global-linum-mode 1)
+      (global-hl-line-mode)
+      (setq electric-pair-mode 1)
+      (use-package diff-hl
+:config
+      (global-diff-hl-mode))
+
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(set-window-scroll-bars (minibuffer-window) nil nil) ; minibuffer window has a scroll bar for some reason
+
+(use-package which-key)
+(which-key-mode)
+
+(global-prettify-symbols-mode t)
+(setq column-number-mode t)
+(setq line-number-mode t)
+
+(use-package moody
+  :config
+  (setq x-underline-at-descent-line t)
+  (moody-replace-mode-line-buffer-identification)
+  (moody-replace-vc-mode)
+  (setq size-indication-mode t) ; display how long file is in modeline
+)
+
+(use-package minions
+:config
+(setq minions-mode-line-lighter "Hey Thor ;)" ; because why not
+      minions-mode-line-delimiters '("" . ""))
+(minions-mode 1))
+
+(use-package ivy)
+(use-package swiper) ; search extension to ivy
+(use-package counsel) ;
+(ivy-mode 1) ; globally turn on ivy
+(setq ivy-use-virtual-buffers t) ; variably sized
+(setq ivy-count-format "(%d/%d) ")
+(global-set-key (kbd "C-s") 'swiper-isearch)
+(global-set-key (kbd "C-r") 'swiper-isearch-backward)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "M-y") 'counsel-yank-pop) ; nicer kill ring
+(global-set-key (kbd "C-h f") 'counsel-describe-function)
+(global-set-key (kbd "C-h v") 'counsel-describe-variable)
+(global-set-key (kbd "C-h l") 'counsel-find-library)
+(global-set-key (kbd "C-h i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "C-x b") 'ivy-switch-buffer)
+(global-set-key (kbd "C-c v") 'ivy-push-view)
+(global-set-key (kbd "C-c V") 'ivy-pop-view)
+
+(global-set-key (kbd "C-c k") 'counsel-rg)
+(global-set-key (kbd "C-c j") 'counsel-file-jump)
+
+(global-set-key (kbd "C-c r") 'ivy-resume)
+;(global-set-key (kbd "C-c b") 'counsel-bookmark) ; weird stuff goin on
+(global-set-key (kbd "C-c d") 'counsel-descbinds)
+(global-set-key (kbd "C-c o") 'counsel-outline)
+
+(use-package ivy-rich
+ :config (ivy-rich-mode 1))
+(setcdr (assq t ivy-format-functions-alist) #'ivy-format-function-line) ; recommended format
+(setq ivy-rich-path-style 'abbrev) ; abbreviate paths with ~/
+
+(use-package avy)
+(global-set-key (kbd "M-t") 'avy-goto-word-1)
+
+(use-package projectile)
+(use-package counsel-projectile)
+(counsel-projectile-mode)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+(use-package magit)
+
+(use-package rg)
+(hrs/append-to-path "/usr/local/bin") ; oddly wasn't globally in path, fixing that
+
+(add-hook 'dired-load-hook
+          (lambda ()
+            (load "dired-x")))
+
+(use-package dumb-jump)
+(global-set-key (kbd "C-M-s") 'dumb-jump-go)
+(global-set-key (kbd "C-M-r") 'dumb-jump-back)
+(setq dumb-jump-force-searcher 'rg)
+(setq dumb-jump-selector 'ivy)
+
+(use-package re-builder)
+(setq reb-re-syntax 'string)
+
+(use-package simpleclip)
+(simpleclip-mode 1)
+
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+
+(setq org-src-fontify-natively t)
+(setq org-src-tab-acts-natively t)
+(setq org-adapt-indentation nil)
+(setq org-pretty-entities t) ; quick latex-ify in org files
+
+(setq org-directory "~/org")
+	(setq org-todo-keywords									; ! = timestamp, @ = create note
+				'((sequence "TODO(t!)" "NOW(n!)" "WAITING(w)" "|" "DONE(d)" "CANCELED(c)")))
+(setq org-log-done 'time) ; log when finished
+
+(use-package org-download)
+
+(setq org-default-notes-file (concat org-directory "~/org/tktodos.org")) ; capture
+	(setq org-capture-templates
+				 '(
+		 ("z" "Misc todo" entry (file+headline "~/org/misc.org" "Misc")
+			"* TODO \t %? :MISC:\nAdded: %u:" :empty-lines 1 )
+		 ("d" "Dev" entry (file+headline "~/org/dev.org" "Dev")
+			"* TODO \t %? :DEV:\nAdded: %u" :empty-lines 1 )
+		 ("M" "Main Dev" entry (file+headline "~/org/main.org" "Main")
+			"* TODO [#A] \t %? :MAIN:DEV:\nAdded: %u" :empty-lines 1 )
+		 ("R" "Main Rsch" entry (file+headline "~/org/main.org" "Main")
+			"* TODO [#A] \t %? :MAIN::RSCH:\nAdded: %u" :empty-lines 1 )
+		 ("e" "Emacs" entry (file+headline "~/org/emacs.org" "Emacs")
+			"* TODO \t %? :EMACS:\nAdded: %u" :empty-lines 1 )
+		 ("p" "Personal" entry (file+headline "~/org/pers.org" "Pers")
+			"* TODO \t %? :PERS:\nAdded: %u" :empty-lines 1 )
+		 ("r" "Research" entry (file+headline "~/org/rsch.org" "Rsch")
+			"* TODO \t %? :RSCH:\nAdded: %u" :empty-lines 1 )
+		 ("i" "Idea" entry (file "~/org/ideas.org")
+			"* \t %? :IDEA:\nAdded: %u" )
+))
+
+(setq org-clock-idle-teme 15) ;prompt after 15 idle minutes.
+
+(setq org-agenda-files '("~/org" ))
+
+; tf not used heavily atm
+			(setq org-agenda-custom-commands ; options - todo, tags, tags-todo
+						'(("d" "Dev" tags-todo "DEV")
+							("e" "Emacs" tags-todo "EMACS")
+							("p" "Personal" tags-todo "PERS")
+							("r" "Research" tags-todo "RSCH")
+							("m" "Research" tags-todo "MAIN")
+))
+(setq org-agenda-start-on-weekday nil) ; start today
+
+(org-agenda nil "a")
+
+(setq org-columns-default-format "%50ITEM(Task) %10CLOCKSUM %16TIMESTAMP_IA")
+
+(setq org-tag-alist '(("dev" . d) ("personal" . ?p) ("research" . ?r) ("main" . ?m)))
+
+(use-package company-org-roam)
+  (setq org-roam-completion-system 'ivy)
+	(use-package org-roam
+				:hook
+				(after-init . org-roam-mode)
+				:custom ; adjust graph dot executable
+				(org-roam-directory "~/org/roam")
+				(setq org-roam-tag-sources '(prop all-directories)) ; tag all intermediate dirs
+				:bind (:map org-roam-mode-map
+								(("C-c n l" . org-roam)
+								 ("C-c n f" . org-roam-find-file)
+								 ("C-c n g" . org-roam-graph))
+								:map org-mode-map
+								(("C-c n i" . org-roam-insert))
+								(("C-c n c" . org-roam-capture))
+;								(("C-c n I" . org-roam-insert-immediate))
+))
+
+(setq org-roam-capture--file-name-default "<%Y-%m%-%d>")
+ (setq org-roam-capture-templates
+				'(("t" "temp" plain (function org-roam--capture-get-point)
+					 "%?"
+					 :file-name "lit/${slug}"
+					 :head: "#+title: ${title}\n"
+					 :unnarrowed t)
+				("l" "lit" plain (function org-roam--capture-get-point)
+				 "%?"
+				 :file-name "lit/${slug}"
+			   :head "#+title: ${title}\n"
+				 :unnarrowed t)))
+ (setq org-roam-capture-ref-templates ; unclear what this is doing
+				'(("r" "ref" plain (function org-roam-capture--get-point)
+					 "%?"
+					 :file-name "lit/${slug}"
+					 :head "#+title: ${title}
+- source :: ${ref}"
+					 :unnarrowed t)))
+
+(setq org-roam-graph-executable "/usr/local/bin/dot")
+(use-package graphviz-dot-mode
+  :config
+(setq graphviz-dot-indent-width 4))
+(setq org-roam-graph-viewer "/Applications/Safari.app/Contents/MacOS/safari")
+
+
+
+(setq-default tab-width 2)
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode)) ; test
+
+(use-package cargo)
+
+(use-package rust-mode
+  :config
+	      (hrs/append-to-path "/Users/thor/.cargo/bin")
+  (setq rust-format-on-save t))
+
+(use-package racer)
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode) ; shows in echo area the arg list of the fn at point
+(add-hook 'racer-mode-hook #'company-mode) ; company autocomplete sometimes slows editor down significantly
+(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+(setq company-tooltip-align-annotations t)
+
+(use-package rust-playground)
+
+(use-package ob-rust)
+
+(use-package flycheck-rust) ; runs on save buffer
+(with-eval-after-load 'rust-mode
+		(add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
+(add-hook 'rust-mode-hook
+		(lambda ()
+		(setq cargo-minor-mode t) ; Cc Cc C(b/r/t)
+		(local-set-key (kbd "C-M-c p") 'rust-debug-println)
+		(local-set-key (kbd "C-h C-h") 'racer-describe-tooltip)
+	))
+
+(fset 'rust-debug-println
+   "println!(\"debug:{:?}\",\C-f\;\C-b\C-b")
+
+(defun rust-sanity-println (var)
+  "check my sanity with a println"
+(interactive)
+)
+
+(defun rust-comment-sanity-checks ()
+  "comment out all sanity printlns"
+  (interactive))
+
+(use-package go-mode)
+(use-package go-errcheck)
+(use-package company) ; autocompletes
+(use-package company-go)
+(setq company-tooltip-limit 20)     ; bigger popup window
+(setq company-idle-delay .2)        ; decrease delay before autocompletion popup shows
+
+(setenv "GOPATH" "/Users/thor/go")
+	(hrs/append-to-path (concat (getenv "GOPATH") "/bin")) ; user gopath
+;	(hrs/append-to-path "/usr/local/go/bin") ; other shit that we like
+
+(setq gofmt-command "goimports")
+(add-hook 'before-save-hook 'gofmt-before-save)
+
+(add-hook 'go-mode-hook
+						(lambda ()
+							(if (not (string-match "go" compile-command))
+									(set (make-local-variable 'compile-command)
+										 "go run ")
+								)))
+
+(add-hook 'go-mode-hook
+					(lambda ()
+						(set (make-local-variable 'company-backends)
+								 '(company-go))
+						(company-mode)
+						(flycheck-mode)
+						(local-set-key (kbd "C-c C-c C-r") 'compile)
+	))
+
+(use-package paredit)
+(use-package rainbow-delimiters)
+
+(setq lispy-mode-hooks
+      '(emacs-lisp-mode-hook
+        lisp-mode-hook
+        scheme-mode-hook))
+(dolist (hook lispy-mode-hooks)
+  (add-hook hook (lambda ()
+                   (setq show-paren-style 'expression)
+                   (paredit-mode)
+                   (rainbow-delimiters-mode))))
+
+(use-package eldoc
+  :config
+  (add-hook 'emacs-lisp-mode-hook 'eldoc-mode))
+
+(use-package flycheck-package)
+(eval-after-load 'flycheck
+  '(flycheck-package-setup))
+
+;  (use-package auctex)
+; Error (use-package): ; auctex/:catch: Loading file /Users/thor/.emacs.d/elpa/auctex-12.2.4/auctex.elc failed to provide feature auctex
+	(setq TeX-auto-save t)
+	(setq TeX-parse-self t)
+	(setq-default TeX-master nil)
+	(add-hook 'LaTeX-mode-hook 'visual-line-mode) ; an altern to auto-fill-mode
+	(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+	(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+	(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+	(setq reftex-plug-into-AUCTeX t)
+	(add-hook 'tex-mode-hook
+						(lambda ()
+						(latex-electric-env-pair-mode)))
---
+++ init.el
@@ -0,0 +1,122 @@
+(require 'package)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
+
+;; Ensure that use-package is installed.
+;;
+;; If use-package isn't already installed, it's extremely likely that this is a
+;; fresh installation! So we'll want to update the package repository and
+;; install use-package before loading the literate configuration.
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file "~/.emacs.d/config.org")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+	 [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+	 ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(compilation-message-face (quote default))
+ '(cua-global-mark-cursor-color "#93E0E3")
+ '(cua-normal-cursor-color "#DCDCCC")
+ '(cua-overwrite-cursor-color "#F0DFAF")
+ '(cua-read-only-cursor-color "#7F9F7F")
+ '(custom-safe-themes
+	 (quote
+		("7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "0fffa9669425ff140ff2ae8568c7719705ef33b7a927a0ba7c5e2ffcfac09b75" "c5ad91387427abc66af38b8d6ea74cade4e3734129cbcb0c34cc90985d06dcb3" "2d835b43e2614762893dc40cbf220482d617d3d4e2c35f7100ca697f1a388a0e" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "13a8eaddb003fd0d561096e11e1a91b029d3c9d64554f8e897b2513dbf14b277" "00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" default)))
+ '(electric-pair-mode t)
+ '(electric-pair-pairs
+	 (quote
+		((96 . 96)
+		 (91 . 93)
+		 (123 . 125)
+		 (40 . 41)
+		 (34 . 34)
+		 (8216 . 8217)
+		 (8220 . 8221))))
+ '(highlight-changes-colors (quote ("#DC8CC3" "#bbb0cb")))
+ '(highlight-symbol-colors
+	 (quote
+		("#680f63eb5998" "#54db645064d0" "#6097535f5322" "#5c2859a95fa1" "#4ede55f24ea4" "#64dd5979525e" "#530060d16157")))
+ '(highlight-symbol-foreground-color "#FFFFEF")
+ '(highlight-tail-colors
+	 (quote
+		(("#4F4F4F" . 0)
+		 ("#488249" . 20)
+		 ("#5dacaf" . 30)
+		 ("#57a2a4" . 50)
+		 ("#b6a576" . 60)
+		 ("#ac7b5a" . 70)
+		 ("#aa5790" . 85)
+		 ("#4F4F4F" . 100))))
+ '(hl-bg-colors
+	 (quote
+		("#b6a576" "#ac7b5a" "#9f5c5c" "#aa5790" "#85749c" "#57a2a4" "#5dacaf" "#488249")))
+ '(hl-fg-colors
+	 (quote
+		("#3F3F3F" "#3F3F3F" "#3F3F3F" "#3F3F3F" "#3F3F3F" "#3F3F3F" "#3F3F3F" "#3F3F3F")))
+ '(hl-paren-colors (quote ("#93E0E3" "#F0DFAF" "#8CD0D3" "#bbb0cb" "#7F9F7F")))
+ '(notmuch-identities (quote ("thor314@gmail.com")))
+ '(notmuch-search-oldest-first nil)
+ '(notmuch-show-logo nil)
+ '(nrepl-message-colors
+	 (quote
+		("#CC9393" "#DFAF8F" "#F0DFAF" "#488249" "#95d291" "#57a2a4" "#93E0E3" "#DC8CC3" "#bbb0cb")))
+ '(org-roam-directory "~/org/roam")
+ '(package-selected-packages
+	 (quote
+		(company-shell notmuch company-org-roam company-graphviz-dot graphviz-dot-mode auctex which-key use-package solarized-theme simpleclip rust-playground rg rainbow-delimiters racer py-autopep8 paredit ob-rust moody minions magit ivy-rich go-errcheck flycheck-rust flycheck-package elpy dumb-jump diff-hl deadgrep counsel-projectile company-jedi company-go cargo avy auto-compile)))
+ '(setq "%<%Y%m%d%H%M%S>" t)
+ '(user-mail-address "thor314@gmail.com"))
+ '(package-selected-packages
+	 (quote
+		(auctex ivy-rich which-key travis htmlize counsel-projectile projectile counsel swiper ivy ripgrep rg flycheck-inline-mode dumb-jump rust-playground ob-rust racer cargo company-rust deadgrep python-mode haskell-mode diff-hl minions solarized-theme simpleclip magit rust-mode flycheck-package rainbow-delimiters paredit flycheck use-package go-errcheck company-go auto-compile)))
+ '(pos-tip-background-color "#4F4F4F")
+ '(pos-tip-foreground-color "#FFFFEF")
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#7F9F7F" "#4F4F4F" 0.2))
+ '(term-default-bg-color "#3F3F3F")
+ '(term-default-fg-color "#DCDCCC")
+ '(vc-annotate-background nil)
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+	 (quote
+		((20 . "#CC9393")
+		 (40 . "#df51b97ca1ae")
+		 (60 . "#e83dcc9aa8b1")
+		 (80 . "#F0DFAF")
+		 (100 . "#cadbca369f51")
+		 (120 . "#b7fbbf79973e")
+		 (140 . "#a52cb4cc8f3f")
+		 (160 . "#9260aa2d8754")
+		 (180 . "#7F9F7F")
+		 (200 . "#87dbb4dba003")
+		 (220 . "#8b6ebfadb0a1")
+		 (240 . "#8e96ca9fc17c")
+		 (260 . "#914ed5b0d293")
+		 (280 . "#93E0E3")
+		 (300 . "#90c5da6cdd6f")
+		 (320 . "#8f5dd735da39")
+		 (340 . "#8df4d401d704")
+		 (360 . "#8CD0D3"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+	 (quote
+		(unspecified "#3F3F3F" "#4F4F4F" "#9f5c5c" "#CC9393" "#488249" "#7F9F7F" "#b6a576" "#F0DFAF" "#57a2a4" "#8CD0D3" "#aa5790" "#DC8CC3" "#5dacaf" "#93E0E3" "#DCDCCC" "#6F6F6F")))
+ '(xterm-color-names
+	 ["#4F4F4F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#fffff6"])
+ '(xterm-color-names-bright
+	 ["#3F3F3F" "#DFAF8F" "#878777" "#6F6F6F" "#DCDCCC" "#bbb0cb" "#FFFFEF" "#FFFFFD"])
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
