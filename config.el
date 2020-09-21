0d4eb9e0d69b9949125f2eaab5c299e036a2e177
Thor Kamphefner <thork@tuta.io>
2020-09-21 14:43:06 +0200

    debugging

--- config.el
+++ config.el
@@ -144,74 +144,6 @@
 (use-package zoom
 	:config (zoom-mode t))
 
-(use-package centaur-tabs
-	 :demand ;; don't defer load, recommended
- :config
-	(setq centaur-tabs-style "bar"
-	 centaur-tabs-height 32
-	 centaur-tabs-set-icons t
-	 centaur-tabs-set-modified-marker t
-	 centaur-tabs-show-navigation-buttons t
-	 centaur-tabs-set-bar 'left)
-	(centaur-tabs-headline-match)
-	;; (centaur-tabs-enable-buffer-reordering)
-	;; (setq centaur-tabs-adjust-buffer-order t)
-	(centaur-tabs-mode t)
-(setq uniquify-separator "/")
-	(setq uniquify-buffer-name-style 'forward)
-	(defun centaur-tabs-buffer-groups () ;; straight from source
-		"`centaur-tabs-buffer-groups' control buffers' group rules.
-Group centaur-tabs with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
-All buffer name start with * will group to \"Emacs\".
-Other buffer group by `centaur-tabs-get-group-name' with project name."
-		(list
-		 (cond
- ;; ((not (eq (file-remote-p (buffer-file-name)) nil))
- ;; "Remote")
- ((or (string-equal "*" (substring (buffer-name) 0 1))
-			(memq major-mode '(magit-process-mode
-			 magit-status-mode
-			 magit-diff-mode
-			 magit-log-mode
-			 magit-file-mode
-			 magit-blob-mode
-			 magit-blame-mode
-			 )))
-	"Emacs")
- ((derived-mode-p 'prog-mode)
-	"Editing")
- ((derived-mode-p 'dired-mode)
-	"Dired")
- ((memq major-mode '(helpful-mode
-				 help-mode))
-	"Help")
- ((memq major-mode '(org-mode
-				 org-agenda-clockreport-mode
-				 org-src-mode
-				 org-agenda-mode
-				 org-beamer-mode
-				 org-indent-mode
-				 org-bullets-mode
-				 org-cdlatex-mode
-				 org-agenda-log-mode
-				 diary-mode))
-	"OrgMode")
- (t
-	(centaur-tabs-get-group-name (current-buffer))))))
-	:hook
-	(dashboard-mode . centaur-tabs-local-mode)
-	(term-mode . centaur-tabs-local-mode)
-	(calendar-mode . centaur-tabs-local-mode)
-	(org-agenda-mode . centaur-tabs-local-mode)
-	(helpful-mode . centaur-tabs-local-mode)
-	 :bind
-	 (("C-M-'" . centaur-tabs-backward)
-		("C-M-," . centaur-tabs-forward)
-	("C-c t s" . centaur-tabs-counsel-switch-group)
-	("C-c t p" . centaur-tabs-group-by-projectile-project)
-	("C-c t g" . centaur-tabs-group-buffer-groups)
-	))
-
 (setq subword-mode t)
 
 (add-hook 'text-mode-hook 'turn-on-auto-fill) ;test
@@ -227,5 +159,316 @@ Other buffer group by `centaur-tabs-get-group-name' with project name."
 (scroll-bar-mode 0)
 (set-window-scroll-bars (minibuffer-window) nil nil) ; minibuffer window has a scroll bar for some reason
 
-;(use-package which-key)
-;(which-key-mode)
+(global-prettify-symbols-mode t)
+(setq column-number-mode t)
+(setq line-number-mode t)
+
+(use-package moody
+  :config
+  (setq x-underline-at-descent-line t)
+  (moody-replace-mode-line-buffer-identification)
+  (moody-replace-vc-mode)
+  (setq size-indication-mode t) ; display how long file is in modeline
+)
+
+(use-package minions
+:config
+(setq minions-mode-line-lighter "Hey Thor ;)" ; because why not
+      minions-mode-line-delimiters '("" . ""))
+(minions-mode 1))
+
+(dolist (hook '(text-mode-hook)) ; when entering text mode
+			(add-hook hook (lambda () (flyspell-mode 1)))) ; add hook to turn on flyspell
+;(add-hook 'prog-mode-hook ; turn on flyspell in comments of programming modes
+;					(lambda ()
+;						(flyspell-prog-mode)
+;					))
+(setq flyspell-issue-message-flag nil) ; printing messages for every word slows down perf
+
+(use-package workgroups2
+	:config
+(setq wg-session-file "~/.emacs.d/workgroups")
+(setq wg-emacs-exit-save-behavior 'save))      ; Options: 'save 'ask nil
+(setq wg-prefix-key (kbd "C-c z"))
+(workgroups-mode)
+
+(use-package ivy)
+(use-package swiper) ; search extension to ivy
+(use-package counsel) ;
+(ivy-mode 1) ; globally turn on ivy
+(setq ivy-use-virtual-buffers t) ; variably sized
+(setq ivy-count-format "(%d/%d) ")
+(global-set-key (kbd "C-s") 'swiper-isearch)
+(global-set-key (kbd "C-r") 'swiper-isearch-backward)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "M-y") 'counsel-yank-pop) ; nicer kill ring
+(global-set-key (kbd "C-h f") 'counsel-describe-function)
+(global-set-key (kbd "C-h v") 'counsel-describe-variable)
+(global-set-key (kbd "C-h l") 'counsel-find-library)
+(global-set-key (kbd "C-h i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "C-x b") 'ivy-switch-buffer)
+(global-set-key (kbd "C-c v") 'ivy-push-view)
+(global-set-key (kbd "C-c V") 'ivy-pop-view)
+
+(global-set-key (kbd "C-c k") 'counsel-rg)
+(global-set-key (kbd "C-c j") 'counsel-file-jump)
+
+(global-set-key (kbd "C-c r") 'ivy-resume)
+;(global-set-key (kbd "C-c b") 'counsel-bookmark) ; weird stuff goin on
+(global-set-key (kbd "C-c o") 'counsel-outline)
+
+(use-package ivy-rich
+ :config (ivy-rich-mode 1))
+(setcdr (assq t ivy-format-functions-alist) #'ivy-format-function-line) ; recommended format
+(setq ivy-rich-path-style 'abbrev) ; abbreviate paths with ~/
+
+(use-package avy)
+(global-set-key (kbd "M-t") 'avy-goto-word-1)
+
+(use-package projectile)
+(use-package counsel-projectile)
+(counsel-projectile-mode)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+(use-package magit)
+
+(use-package rg)
+(hrs/append-to-path "/usr/local/bin") ; oddly wasn't globally in path, fixing that
+
+(add-hook 'dired-load-hook
+          (lambda ()
+            (load "dired-x")))
+
+(use-package dumb-jump)
+(global-set-key (kbd "C-M-s") 'dumb-jump-go)
+(global-set-key (kbd "C-M-r") 'dumb-jump-back)
+(setq dumb-jump-force-searcher 'rg)
+(setq dumb-jump-selector 'ivy)
+
+(use-package re-builder)
+(setq reb-re-syntax 'string)
+
+(use-package simpleclip)
+(simpleclip-mode 1)
+
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+
+(setq org-src-fontify-natively t)
+(setq org-src-tab-acts-natively t)
+(setq org-adapt-indentation nil)
+;(setq org-pretty-entities nil) ; quick latex-ify in org files; annoying in codesnippets
+
+(setq org-directory "~/org")
+	(setq org-todo-keywords									; ! = timestamp, @ = create note
+				'((sequence "TODO(t!)" "NOW(n!)" "WAITING(w)" "|" "DONE(d)" "CANCELED(c)")))
+(setq org-log-done 'time) ; log when finished
+
+(use-package org-download)
+
+(setq org-default-notes-file (concat org-directory "~/org/tktodos.org")) ; capture
+	(setq org-capture-templates
+				 '(
+		 ("z" "Misc todo" entry (file+headline "~/org/misc.org" "Misc")
+			"* TODO \t %? :MISC:\nAdded: %u:" :empty-lines 1 )
+		 ("d" "Dev" entry (file+headline "~/org/dev.org" "Dev")
+			"* TODO \t %? :DEV:\nAdded: %u" :empty-lines 1 )
+		 ("M" "Main Dev" entry (file+headline "~/org/main.org" "Main")
+			"* TODO [#A] \t %? :MAIN:DEV:\nAdded: %u" :empty-lines 1 )
+		 ("R" "Main Rsch" entry (file+headline "~/org/main.org" "Main")
+			"* TODO [#A] \t %? :MAIN:RSCH:\nAdded: %u" :empty-lines 1 )
+		 ("e" "Emacs" entry (file+headline "~/org/emacs.org" "Emacs")
+			"* TODO \t %? :EMACS:\nAdded: %u" :empty-lines 1 )
+		 ("p" "Personal" entry (file+headline "~/org/pers.org" "Pers")
+			"* TODO \t %? :PERS:\nAdded: %u" :empty-lines 1 )
+		 ("r" "Research" entry (file+headline "~/org/rsch.org" "Rsch")
+			"* TODO \t %? :RSCH:\nAdded: %u" :empty-lines 1 )
+		 ("i" "Idea" entry (file "~/org/ideas.org")
+			"* \t %? :IDEA:\nAdded: %u" )
+))
+
+(add-hook 'org-mode-hook
+			(lambda ()
+				(local-set-key (kbd "C-c C-x C-l") 'org-clock-in-last)
+))
+
+(setq org-clock-idle-teme 15) ;prompt after 15 idle minutes.
+
+(setq org-agenda-files '("~/org" ))
+
+; tf not used heavily atm
+			(setq org-agenda-custom-commands ; options - todo, tags, tags-todo
+						'(("d" "Dev" tags-todo "DEV")
+							("e" "Emacs" tags-todo "EMACS")
+							("p" "Personal" tags-todo "PERS")
+							("r" "Research" tags-todo "RSCH")
+							("m" "Research" tags-todo "MAIN")
+))
+(setq org-agenda-start-on-weekday nil) ; start today
+
+(setq org-columns-default-format "%50ITEM(Task) %10CLOCKSUM %16TIMESTAMP_IA")
+
+(setq org-tag-alist '(("dev" . d) ("personal" . ?p) ("research" . ?r) ("main" . ?m)))
+
+(use-package company-org-roam)
+  (setq org-roam-completion-system 'ivy)
+	(use-package org-roam
+				:hook
+				(after-init . org-roam-mode)
+				:custom ; adjust graph dot executable
+				(org-roam-directory "~/org/roam")
+				(setq org-roam-tag-sources '(prop all-directories)) ; tag all intermediate dirs
+				:bind (:map org-roam-mode-map
+								(("C-c n l" . org-roam)
+								 ("C-c n f" . org-roam-find-file)
+								 ("C-c n g" . org-roam-graph))
+								:map org-mode-map
+								(("C-c n i" . org-roam-insert))
+								(("C-c n c" . org-roam-capture))
+;								(("C-c n I" . org-roam-insert-immediate))
+))
+
+(setq org-roam-capture--file-name-default "<%Y-%m%-%d>")
+	 (setq org-roam-capture-templates
+					'(("p" "paper" plain (function org-roam--capture-get-point)
+						 "%?"
+						 :file-name "paper/${topic}/${subtopic}/${slug}"
+						 :head: "#+title: ${title}\n"
+						 :unnarrowed t)
+					("w" "web" plain (function org-roam--capture-get-point)
+					 "%?"
+					 :file-name "web/${topic}/${subtopic}/${slug}"
+					 :head "#+title: ${title}\n"
+					 :unnarrowed t)
+					("b" "book" plain (function org-roam--capture-get-point)
+					 "%?"
+					 :file-name "book/${topic}/${slug}"
+					 :head "#+title: ${title}\n"
+					 :unnarrowed t)
+))
+
+(setq org-roam-graph-executable "/usr/local/bin/dot")
+(use-package graphviz-dot-mode
+  :config
+(setq graphviz-dot-indent-width 4))
+(setq org-roam-graph-viewer "/Applications/Safari.app/Contents/MacOS/safari")
+
+
+
+(setq-default tab-width 2)
+
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode)) ; test
+
+(use-package cargo)
+
+(use-package rust-mode
+  :config
+	(hrs/append-to-path "~/.cargo/bin")
+  (setq rust-format-on-save t))
+
+(use-package racer
+	:config (setq company-tooltip-align-annotations t)
+	:hook ((rust-mode . racer-mode)
+	(add-racer-mode . eldoc-mode) ; shows in echo area the arg list of the fn at point
+	(racer-mode . company-mode)) ; company autocomplete sometimes slows editor down significantly
+	:bind (:map rust-mode-map ("TAB" . company-indent-or-complete-common)))
+;	(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+
+(use-package rust-playground)
+
+(use-package ob-rust)
+
+(use-package flycheck-rust) ; runs on save buffer
+(with-eval-after-load 'rust-mode
+		(add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
+(add-hook 'rust-mode-hook
+		(lambda ()
+		(setq cargo-minor-mode t) ; Cc Cc C(b/r/t)
+		(local-set-key (kbd "C-M-c p") 'rust-debug-println)
+		(local-set-key (kbd "C-h C-h") 'racer-describe-tooltip)
+	))
+
+(fset 'rust-debug-println
+   "println!(\"debug:{:?}\",\C-f\;\C-b\C-b")
+
+(defun rust-sanity-println (var)
+  "check my sanity with a println"
+(interactive)
+)
+
+(defun rust-comment-sanity-checks ()
+  "comment out all sanity printlns"
+  (interactive))
+
+(use-package go-mode)
+(use-package go-errcheck)
+(use-package company) ; autocompletes
+(use-package company-go)
+(setq company-tooltip-limit 20)     ; bigger popup window
+(setq company-idle-delay .2)        ; decrease delay before autocompletion popup shows
+
+(setenv "GOPATH" "/Users/thor/go")
+(hrs/append-to-path (concat (getenv "GOPATH") "/bin")) ; user gopath
+(hrs/append-to-path "/usr/local/go/bin") ; other shit that we like
+
+(setq gofmt-command "goimports")
+(add-hook 'before-save-hook 'gofmt-before-save)
+
+(add-hook 'go-mode-hook
+						(lambda ()
+							(if (not (string-match "go" compile-command))
+									(set (make-local-variable 'compile-command)
+										 "go run ")
+								)))
+
+(add-hook 'go-mode-hook
+					(lambda ()
+						(set (make-local-variable 'company-backends)
+								 '(company-go))
+						(company-mode)
+						(flycheck-mode)
+						(local-set-key (kbd "C-c C-c C-r") 'compile)
+	))
+
+(use-package paredit)
+(use-package rainbow-delimiters)
+
+(setq lispy-mode-hooks
+      '(emacs-lisp-mode-hook
+        lisp-mode-hook
+        scheme-mode-hook))
+(dolist (hook lispy-mode-hooks)
+  (add-hook hook (lambda ()
+                   (setq show-paren-style 'expression)
+                   (paredit-mode)
+                   (rainbow-delimiters-mode))))
+
+(use-package eldoc
+  :config
+  (add-hook 'emacs-lisp-mode-hook 'eldoc-mode))
+
+(use-package flycheck-package)
+(eval-after-load 'flycheck
+  '(flycheck-package-setup))
+
+(use-package company-shell)
+(add-to-list 'company-backends 'company-shell)
+
+;  (use-package auctex)
+; Error (use-package): ; auctex/:catch: Loading file /Users/thor/.emacs.d/elpa/auctex-12.2.4/auctex.elc failed to provide feature auctex
+	(setq TeX-auto-save t)
+	(setq TeX-parse-self t)
+	(setq-default TeX-master nil)
+	(add-hook 'LaTeX-mode-hook 'visual-line-mode) ; an altern to auto-fill-mode
+	(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+	(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+	(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+	(setq reftex-plug-into-AUCTeX t)
+	(add-hook 'tex-mode-hook
+						(lambda ()
+						(latex-electric-env-pair-mode)))
